c839aa047e0d92266c249a668bd4e4d2a1432a29
Nayeon Chae <cny1230@naver.com>
2024-04-15 15:00:58 +0900

    final

---
+++ wishList/wishList/Model/RemoteProduct.swift
@@ -0,0 +1,21 @@
+//
+//  RemoteProduct.swift
+//  wishList
+//
+//  Created by 채나연 on 4/12/24.
+//
+
+import Foundation
+
+// URL Session을 통해 가져올 상품의 Decodable Model 입니다.
+
+
+struct RemoteProduct: Decodable {
+    let id: Int
+    let title: String
+    let description: String
+    let price: Double
+    let thumbnail: URL
+}
+
+
